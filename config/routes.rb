f45b6c8c88db89057ca30eae525527bbbd75f1ab
latinman <erivas78@gmail.com>
2015-12-20 23:00:50 -0500

    Fix user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "user/registrations" }
   resources :user_stocks, except: [:edit, :update, :show]
   root "welcome#index"
   get "my_portfolio" => "users#my_portfolio"
